95e60142e60b9f216bd06a0832b7142039e086ca
sam mackenzie <samtmackenzie@gmail.com>
2020-04-06 14:04:04 -0400

    LCD branch

--- thermo_fw/Core/Src/main.c
+++ thermo_fw/Core/Src/main.c
@@ -17,6 +17,13 @@
   *
   *
   *
+  * Additional code Copyright (c) System 2 Neurotechnology Inc.
+  *   *
+  * This software component is licensed by System 2 Neurotechnology Inc.
+  * under BSD 3-Clause license, the "License"; You may not use this file
+  * except in compliance with the License. You may obtain a copy of the
+  * License at:
+  *                        opensource.org/licenses/BSD-3-Clause
   *
   *
   ******************************************************************************
--- thermo_fw/Core/Src/thermo.c
+++ thermo_fw/Core/Src/thermo.c
@@ -1,10 +1,18 @@
-/*
- * thermo.c
- *
- *  Created on: Mar 26, 2020
- *      Author: samtm
- */
-
+/*******************************************************************************
+  * thermo.c
+  *
+  *  Created on: Mar 26, 2020
+  *      Author: samtm
+  * Additional code Copyright (c) System 2 Neurotechnology Inc.
+  *   *
+  * This software component is licensed by System 2 Neurotechnology Inc.
+  * under BSD 3-Clause license, the "License"; You may not use this file
+  * except in compliance with the License. You may obtain a copy of the
+  * License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  *
+  ******************************************************************************/
 
 #include "main.h"
 #include "thermo.h"
